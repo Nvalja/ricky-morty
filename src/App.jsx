926c9f33056c7d5b0b4565a36d58981ba753bee2
Nazar Valchyshyn <nvalja@gmail.com>
2021-02-05 18:19:34 +0200

    start work

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>Hello</h1>
       </header>
     </div>
   );
